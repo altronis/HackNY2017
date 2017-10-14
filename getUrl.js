3a4dfa9ec8b9cda588f7cfb22814f955e7720fd5
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-10-14 18:56:30 -0400

    node js implementation

---
+++ getUrl.js
@@ -0,0 +1,16 @@
+function query(q){
+    request.get({
+    url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+    qs: {
+        'api-key': "223293e7d64544aa8e7eef843508be06",
+        'q': q
+    },
+    }, function(err, response, body) {
+        body = JSON.parse(body);
+        console.log(body);
+        for(var i = 0; i < 6; i++){
+            urls.push(result.response.docs[i].web_url);   
+        }
+    })
+    process.stdout.write(urls);
+}
